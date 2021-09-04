4d4be1b6f838ca660e8afe63076a845777108771
Wayne Munro <waynemunro@gmail.com>
2021-09-04 17:42:34 +0200

    initial commit

---
+++ automate.ps1
@@ -0,0 +1,20 @@
+git init
+dotnet new gitignore
+#git status
+git add -A
+git commit -m "initial commit"
+git branch PlatformServiceAPI
+git checkout PlatformServiceAPI
+dotnet new webapi -n PlatformService --force
+dotnet build .\PlatformService
+git add -A
+git commit -m "PlatformService Api initial commit"
+cd .\PlatformService\
+dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Design
+git add -A
+git commit -m "Nuget Package dependencies added commit"
+code -r PlatformService 
+
+
